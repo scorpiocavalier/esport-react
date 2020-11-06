45450ec19e5dd8215612bc365064df1b45d7829c
Arturo Chen <art.can.fly@gmail.com>
2020-11-06 02:09:18 -0500

    + show submenu on menu item hover

--- src/components/Nav/Menu/MenuDropdown.js
+++ src/components/Nav/Menu/MenuDropdown.js
@@ -7,7 +7,7 @@ export function Menudropdown({ open }) {
       <List>
         <ListItem>
           <ItemName>TEAM</ItemName>
-          <Sublist open={ open } isHovered={ true }>
+          <Sublist>
             <SublistItem>Player Name 1</SublistItem>
             <SublistItem>Player Name 2</SublistItem>
             <SublistItem>Player Name 3</SublistItem>
@@ -18,7 +18,7 @@ export function Menudropdown({ open }) {
 
         <ListItem>
           <ItemName>SHOP</ItemName>
-          <Sublist open={ open } isHovered={ false }>
+          <Sublist>
             <SublistItem>Caps</SublistItem>
             <SublistItem>Hoodies</SublistItem>
             <SublistItem>T-Shirts</SublistItem>
@@ -39,7 +39,6 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  visibility: ${ props => props.open ? 'visible' : 'hidden' };
   width: 100vw;
   height: 90vh;
   position: absolute;
@@ -47,6 +46,7 @@ const Wrapper = styled.div`
   left: 0;
   background: black;
   z-index: 10;
+  visibility: ${ props => props.open ? 'visible' : 'hidden' };
   transition: visibility 0.2s ease-out;
 `
 
@@ -54,7 +54,6 @@ const List = styled.ul`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: center;
   width: 60%;
   height: 60%;
   /* border: solid pink; */
@@ -63,23 +62,29 @@ const List = styled.ul`
 const ListItem = styled.li`
   display: flex;
   width: 100%;
+  position: relative;
   /* border: solid green; */
 `
 
 const ItemName = styled.span`
   padding-bottom: 10px;
+  line-height: 1;
   width: 50%;
   font-size: 5em;
   letter-spacing: 10px;
   font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  cursor: pointer;
+
+  &:hover + ul {
+    visibility: visible;
+  }
 `
 
 const Sublist = styled.ul`
-  visibility: ${ props => props.open && props.isHovered ? 'visible' : 'hidden' };
   position: absolute;
   left: 50%;
-  width: 300px;
-  transition: visibility 0.2s ease-out;
+  width: 50%;
+  visibility: hidden;
   /* border: solid pink; */
 `
 
